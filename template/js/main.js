207432774d7e07a813f4279ea1e2ffa46a51fbe7
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2017-11-05 01:12:31 +0300

    05/11 1.12

--- template/js/main.js
+++ template/js/main.js
@@ -10,7 +10,7 @@ $(window).on('load', function () {
 
 $(document).ready(function() {
     $('body .section').eq(0).addClass("active").fadeIn(1000); // Показываем первый блок, можно и не первый, если прописать нужную цифру в eq()
-    setInterval('blockAnimate();', 10000); // Вызываем функцию для смены блока каждые 10 секунд
+    setInterval('blockAnimate();', 20000);// Вызываем функцию для смены блока каждые 20 секунд
 });
 
 // Функция для смены блоков, показывает блоки по очереди, начальный блок задаётся выше
@@ -20,11 +20,35 @@ function blockAnimate() {
         if($(this).hasClass('active') && index != length) {
             $(this).removeClass("active").fadeOut(1000).next('.section').addClass("active").delay(1000).fadeIn(1000);
             return false;
-        } else if (index == length) {
+
+        } 
+        
+        else if (index == length) {
             $(this).removeClass('active').fadeOut(1000);
             $('body .section').eq(0).addClass("active").delay(1000).fadeIn(1000);
             return false;
         }
     });
 };
+
+$(document).ready(function(){
+  $(".menu-btn").click(function() {
+    $(".menu-desktop").fadeToggle(300);
+  });
+});
+$(document).ready(function(){
+  $(".menu-close").click(function() {
+    $(".menu-desktop").fadeToggle(300);
+  });
+});
+$(document).ready(function(){
+  $(".menu-btn").click(function() {
+    $('.menu-close').addClass('change');
+  });
+});
+$(document).ready(function(){
+  $(".menu-close").click(function() {
+    $('.menu-close').removeClass('change');
+  });
+});
 
